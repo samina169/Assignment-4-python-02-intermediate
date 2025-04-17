a3cadaca4ecee7afeca6b4cfdd03bf36afbd430e
samina169 <saminaidrees28@gmail.com>
2025-04-17 12:30:46 +0500

    Add files via upload

---
+++ starter_code.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Title of the app
+st.title("My Streamlit Starter Code App")
+
+# Add a simple text input
+user_input = st.text_input("Enter some text:")
+
+# Display the input back to the user
+st.write("You entered:", user_input)
